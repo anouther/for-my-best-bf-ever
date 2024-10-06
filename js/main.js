ee60fc4d5232a823df595bb5e47cf345bd48c68d
anouther <155570107+anouther@users.noreply.github.com>
2024-10-06 23:02:47 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('GIPY, I LOVE YOU').split('');
+        const titles = ('I LOVE YOU').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Makasih udah mau percaya sama aku buat bikinin hadiah buat kalian kasih ke orang spesial 🤍🤍";
+            subtitle.innerText = "Bunga spesial untuk kamu yang spesial, terimakasih sudah mau bertahan dengan aku sampai sekarang<3";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
